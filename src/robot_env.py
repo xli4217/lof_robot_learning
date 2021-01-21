added entropy loss, fixed scaling in robot_env

src/robot_env.py
@@ -38,9 +38,9 @@ class RobotEnv(gym.Env):
         self.agent_ee_tip = self.agent.get_tip()
         self.initial_joint_positions = self.agent.get_joint_positions()
 
-        action_high = np.array(7 * [1.])
+        action_high = np.array(4 * [1.])
         #action_high[1] = -10.
-        action_low = np.array(7 * [-1.])
+        action_low = np.array(4 * [-1.])
         #action_low[1] = -20
 
         self.action_space = gym.spaces.Box(low=action_low, high=action_high, dtype=np.float32)
@@ -72,24 +72,23 @@ class RobotEnv(gym.Env):
         return self._get_state()
 
     def step(self, action):
-        #action *= 0.5
-        #action[1] *= -1
+        done = False
+        action = np.concatenate([action, np.zeros(3)])
         self.agent.set_joint_target_velocities(action)  # Execute action on arm
         self.pr.step()  # Step the physics simulation
         ax, ay, az = self.agent_ee_tip.get_position()
         tx, ty, tz = self.target.get_position()
+
         # Reward is negative distance to target
-        # print(ax, ay, az)
-        # print(tx, ty, tz)
-        # print("-----")
         r_dist = -np.sqrt((ax - tx) ** 2 + (ay - ty) ** 2 + (az - tz) ** 2)
         r_action = - np.linalg.norm(action)
+        
+        reward = r_dist #+ 0.1*r_action
+        reward *= 0.2
+        if r_dist > -0.05:
+            reward += 0.5
+            done = True
 
-        # print(r_dist)
-        # print(r_action)
-        # print("---------")
-        reward = r_dist + 0.2*r_action
-        done = False
         self.current_step += 1
         if self.current_step >= EPISODE_LENGTH:
             #print(f"episode time: {time.time()-self.t_start}")

src/train.py
@@ -20,12 +20,12 @@ from spinup.algos.pytorch.ppo.ppo import ppo
 HEADLESS = True
 
 logger_kwargs = {
-    "output_dir": os.path.join(os.environ['PKG_PATH'], 'experiments', 'ppo_rlbench'),
-    "exp_name": "ppo_rlbench"
+    "output_dir": os.path.join(os.environ['PKG_PATH'], 'experiments', 'ppo'),
+    "exp_name": "ppo"
 }
 
 ac_kwargs = {
-    'hidden_sizes': (128, 128),
+    'hidden_sizes': (128, 128, 128),
     'activation': torch.nn.Tanh
     #'activation': torch.nn.ReLU
 }
@@ -54,13 +54,15 @@ ppo(env,
     clip_ratio=0.2,
     epochs=300,
     pi_lr=1e-4,
-    vf_lr=2e-4,
-    lam=0.99,
+    vf_lr=5e-4,
+    lam=0.97,
+    gamma=0.995,
     steps_per_epoch=3000,
-    train_pi_iters=10,
+    train_pi_iters=5,
     train_v_iters=10,
-    target_kl=0.01,
-    max_ep_len=200
+    target_kl=0.05,
+    max_ep_len=200,
+    minibatch_size=64
 )
 
 print('Done!')